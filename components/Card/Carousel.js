b3168c467d4656cb586278722a6283a5766d3334
larguijo <rodriguez.arguijo@gmail.com>
2020-11-23 13:41:57 -0600

    Fixing how it look in small devices and adding the first version of the Galery

--- components/Card/Carousel.js
+++ components/Card/Carousel.js
@@ -1,17 +1,4 @@
-import styled from "styled-components";
-
-const ImgContainer = styled.div`
-  padding: 20px 5px;
-
-  img {
-    max-width: 100%;
-    width: auto;
-    height: auto;
-    display: block;
-    margin: auto;
-    padding: 10px;
-  }
-`;
+import ImgContainer from "components/Images/ImgContainer";
 
 function Carousel() {
   return (
